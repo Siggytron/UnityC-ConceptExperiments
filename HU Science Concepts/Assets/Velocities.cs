38c4df57645689cfcda231191f907e051db5fcb6
Siggy <s_gyi@yahoo.com>
2019-02-12 00:22:44 -0500

    Very first commit of the HU Science Concepts folder

---
+++ HU Science Concepts/Assets/Velocities.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocities : MonoBehaviour
+{
+    public float speed;
+
+    public float xMove = 1;
+    public float yMove = 1;
+    public float zMove = 1;
+    
+    private Rigidbody rb;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    rb = GetComponent<Rigidbody>();
+        
+    xMove = Random.value;
+    yMove = Random.value;
+    zMove = Random.value;  
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        xMove = Random.value;
+        yMove = Random.value;
+        zMove = Random.value;
+
+        Vector3 movement = new Vector3(xMove, yMove, zMove);
+        rb.AddForce(movement * speed, ForceMode.Force);
+    }
+
+}
