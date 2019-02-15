c44b0484b29c52c2e2ac943cb468eb13b2b33f30
Siggy <s_gyi@yahoo.com>
2019-02-15 13:54:26 -0500

    Made some visual improvements to the scene. Added invisible colliders in the corners to try to prevent things from sticking in those corners. Started on some code to produce a little force push from those colliders onto any object that collides with them but at the moment that seems to be unnecessary. I can continue down that road though if the problem remains.

---
+++ HU Science Concepts/Assets/Scripts/CollisionInstructions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionInstructions : MonoBehaviour
+{
+    // Apparently it is not enough to refer to materials that have been
+    // created in Unity. They must be created here too, either publicly
+    // or privately before they can be used in the code.
+    public Material Pink;
+    public Material Red;
+    public Material darkBlue;
+    public Material Yellow;
+    Renderer rend;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        print("Start");
+        //Fetch the Renderer from the GameObject
+        rend = GetComponent<Renderer>();
+
+        print(rend.material);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+
+
+
+        if(collision.gameObject.name == "BlueAtom")
+        {
+            rend.material = darkBlue;
+        }
+        else if(collision.gameObject.name == "YellowAtom")
+        {
+            rend.material = Yellow;
+        }
+        else
+        {
+            //rend.material = Pink;
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
---
+++ HU Science Concepts/Assets/Scripts/RepelObjects.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepelObjects : MonoBehaviour
+{
+    // First you have to declare and name a vector. This
+    // one I plan to use to determine the position of whatever
+    // object I have attached this script to.
+    Vector3 boxPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Here is where I get the position of the gameObject
+        // this script is attached to. 
+        boxPosition = transform.position;
+
+        // Print the position to console just to check everything
+        // is kosher.
+        print(boxPosition);
+
+        
+    }
+
+    // This is code is to attempt to keep objects moving randomly from 
+    // sticking in the corners. There may be a way to handle this
+    // more elegantly but it seems a fairly common Unity forum
+    // complaint. This will give a little push away from the corners
+    // to any object that arrives in a corner. This script will be
+    // attached to invisible boxes set in the corners of the 
+    // NavMeshPlane.
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 pointOfContact = collision.contacts[0].point;
+        print(pointOfContact);
+
+        //Vector3 pos = collision.rigidbody.transform;
+        //collision.rigidbody.AddForce(Vector3.Reflect(pos, inNormal));
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
